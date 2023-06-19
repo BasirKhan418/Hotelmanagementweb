1842cb5059ca61f2cd26462f825f8165aae89ec4
Basir Khan <khanbasir5555@gmail.com>
2023-06-19 16:07:38 +0530

    feature fixes

--- src/pages/components/Features.js
+++ src/pages/components/Features.js
@@ -7,9 +7,9 @@ const Features = () => {
 <div className="flex flex-wrap justify-center">
 
     <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+    <span className='flex justify-center items-center'>
         <img src="/f1.jpg" alt="" className="h-20 m-6"/>
-
+</span>
         <h2 className="text-center px-2 pb-5">24/7 Help Desk</h2>  
         
         <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
@@ -19,81 +19,81 @@ const Features = () => {
 
 
     <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+    <span className='flex justify-center items-center'>
         <img src="/f2.png" alt="" className="h-20  m-6"/>
-
+</span>
         <h2 className="text-center px-2 pb-5">Air Conditioning</h2>  
         
         <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
     </div>
     <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+    <span className='flex justify-center items-center'>
 <img src="/f3.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5">Valet Security</h2>  
 
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+<span className='flex justify-center items-center'>
 <img src="/f4.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5"> Service Bell</h2>  
 
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+<span className='flex justify-center items-center'>
 <img src="/f5.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5">Laundry Service</h2>  
 
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+<span className='flex justify-center items-center'>
 <img src="/f6.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5">Free Parking</h2>  
 
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+<span className='flex justify-center items-center'>
 <img src="/f7.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5">BreakFast</h2>  
 
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
-<img src="/f8.png" alt="" className="h-20 m-6"/>
-
+<span className='flex justify-center items-center'>
+<img src="/f8.png" alt="" className="h-20 w-20 m-6"/>
+</span>
 <h2 className="text-center px-2 pb-5">24/7 CCTV Security</h2>  
 
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+<span className='flex justify-center items-center'>
 <img src="/f9.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5">TV</h2>  
 
 <Link href="#" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
 </div>
 <div className="flex flex-col bg-white rounded-lg shadow-md w-full m-6 overflow-hidden sm:w-52">
-
+<span className='flex justify-center items-center'>
 <img src="/f10.png" alt="" className="h-20 m-6"/>
-
+</span>
 <h2 className="text-center px-2 pb-5">Swimming Pool</h2>
 <Link href="/Featurepage" className="bg-amber-500 text-white p-3 text-center hover:bg-amber-800 transition-all duration-500">Know More</Link>
 
